df757bbbc6deab0cc813ea4fbfa1e5d6b36742f1
Felipe Rubilar <frubilar@rhinnoit.cloud>
2020-12-10 10:58:31 -0300

    Agregado directorio media y su configuracion en settings

--- clases/admin.py
+++ clases/admin.py
@@ -3,4 +3,7 @@ from .models import Clase
 
 # Register your models here.
 
-admin.site.register(Clase)
+class ClaseAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+admin.site.register(Clase,ClaseAdmin)
