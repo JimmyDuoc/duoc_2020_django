7c7b074d8710ca83599da0f97dac5929d6882c8d
Felipe Rubilar <frubilar@rhinnoit.cloud>
2020-12-10 13:10:13 -0300

    Agregados instructores y servicios a admin

--- instructores/admin.py
+++ instructores/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Instructor
 
 # Register your models here.
+
+class InstructorAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+admin.site.register(Instructor,InstructorAdmin)
