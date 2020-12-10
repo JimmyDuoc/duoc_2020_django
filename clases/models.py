26dafe1d52f97f1a3e63184241cce95bb37ef46a
jim.zuniga <jimmyzr.as@gmail.com>
2020-12-09 22:07:48 -0300

    problems

--- clases/models.py
+++ clases/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Clase(models.Model):
+    title = models.CharField(max_length = 100, verbose_name = "Título")
+    image = models.ImageField(verbose_name = "Imagen")
+    instructor = models.CharField(max_length = 100, verbose_name = "Instructor")
+    created = models.DateTimeField(auto_now_add = True, verbose_name= "Fecha Creación")
+    updated = models.DateTimeField(auto_now = True, verbose_name= "Fecha Modificación")
+    
+
