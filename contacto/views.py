e65bfc9eac439af6c301ac0dcb004f7972481c9b
Felipe Rubilar <frubilar@rhinnoit.cloud>
2020-12-11 15:25:52 -0300

    Validacion de envio de POST

--- contacto/views.py
+++ contacto/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 from .forms import ContactForm
 
 # Create your views here.
 
 def contacto(request):
     contact_form = ContactForm()
+
+    if request.method == "POST":
+        contact_form = ContactForm(data = request.POST)
+        if contact_form.is_valid():
+            return redirect(reverse('contacto') + "?OK")
+        else:
+            return redirect(reverse('contacto') + "?FAIL")
+
     return render(request,"contacto/contacto.html",{'form':contact_form})
