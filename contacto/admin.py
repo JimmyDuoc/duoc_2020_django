d311f6ecf3ecac240503dcf92dccd8b01d52ffae
Felipe Rubilar <frubilar@rhinnoit.cloud>
2020-12-11 22:45:00 -0300

    Se crea modelo Contacto

--- contacto/admin.py
+++ contacto/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+class ContactoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+admin.site.register(Contacto,ContactoAdmin)
--- contacto/models.py
+++ contacto/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacto(models.Model):
+    first_name = models.CharField(max_length=30, verbose_name="Nombre")
+    last_name models.CharField(max_length=30, verbose_name="Apellido")
+    email = models.CharField(max_length=100, verbose_name="Correo Electrónico")
+    phone = models.CharField(max_length=12, verbose_name="Teléfono")
+    
+    class Meta:
+        verbose_name = "contacto"
+        verbose_name_plural = "contactos"
+        ordering = ['-updated','-created']
+
+    def __str__(self):
+        return self.email
+
