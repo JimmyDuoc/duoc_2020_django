c2c611bb228c238ad9f518d15cbc1668de471b5e
Felipe Rubilar <frubilar@rhinnoit.cloud>
2020-12-11 22:51:14 -0300

    Migraciones contacto

--- contacto/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-class ContactoAdmin(admin.ModelAdmin):
-    readonly_fields = ('created','updated')
-
-admin.site.register(Contacto,ContactoAdmin)
